2edc1347979e7ddd5b985e0cff2fb5b331c67c4f
Taylor Buckner <taylora.buckner@gmail.com>
2020-01-16 12:19:15 -0600

    Format hotfix.

--- projects/material-dayjs-adapter/src/lib/adapter/dayjs-date-formats.ts
+++ projects/material-dayjs-adapter/src/lib/adapter/dayjs-date-formats.ts
@@ -5,10 +5,10 @@ import { MatDateFormats } from '@angular/material/core';
  */
 export const MAT_DAYJS_DATE_FORMATS: MatDateFormats = {
   parse: {
-    dateInput: 'MM.DD.YYYY',
+    dateInput: 'MM/DD/YYYY',
   },
   display: {
-    dateInput: 'MM.DD.YYYY',
+    dateInput: 'MM/DD/YYYY',
     monthYearLabel: 'MMM YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'MMMM YYYY',
