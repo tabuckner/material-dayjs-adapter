45b1fb33e988a793dd36b9894ffdc8cf4cea1644
Taylor Buckner <taylora.buckner@gmail.com>
2020-03-18 13:52:46 -0500

    fix(schematics): fix linter errors in schematics

--- projects/material-dayjs-adapter/schematics/ng-update/index.ts
+++ projects/material-dayjs-adapter/schematics/ng-update/index.ts
@@ -2,10 +2,10 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
-export function ngUpdate(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+export function ngUpdate(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
     throw new Error('Schematic is not yet implemented.');
-    _context.addTask(new NodePackageInstallTask());
+    context.addTask(new NodePackageInstallTask());
     return tree;
   };
 }
